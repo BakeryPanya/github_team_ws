3a14843e8955a171a22cb8d68d709b6283fdb63f
RyotaMiyahara1023 <nakaichi1728@gmail.com>
2022-02-10 20:14:45 +0900

    B2

--- src/index.js
+++ src/index.js
@@ -130,7 +130,9 @@
 	/* 演習1 ここから  */
 	function add(){}
 
-	function sub(){}
+	function sub(){
+		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
+	}
 
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
